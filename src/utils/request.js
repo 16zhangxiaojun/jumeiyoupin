a69d2acf1c265b71f98a4bbba816e35aede31bc4
zhangxiaojun <765845148@qq.com>
2019-11-11 19:51:50 +0800

    项目初始化

---
+++ src/utils/request.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+
+const  server = axios.create({
+    timeout:5000,
+    withCredentials:true    
+})
+
+//请求拦截器
+server.interceptors.request.use((config)=>{
+    if(config.method=="get"){
+        config.params = {...config.data};
+    }
+
+},(err)=>{
+    return Promise.reject(err);
+})
+
+//响应拦截
+server.interceptors.response.use((res)=>{
+    if(res.data  == 200){
+        return res.data;
+    }
+},(err)=>{
+    return Promise.reject(err);
+})
+
+
+export default server;
